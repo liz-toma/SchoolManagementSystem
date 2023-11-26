f1cfe9e1c349ca3f1b423919c8d3be51473a0e90
liz-toma <2369522@edu.vaniercollege.qc.ca>
2023-11-25 22:04:12 -0500

    Main class draft

---
+++ src/main/java/org/liz/Main.java
@@ -0,0 +1,10 @@
+package org.liz;
+
+/**
+ * @author Eliza Toma
+ */
+public class Main {
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+}
