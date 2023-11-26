1b47cc7212ca56e3c9a54ebe9a5d37dfe94cdb2f
liz-toma <2369522@edu.vaniercollege.qc.ca>
2023-11-25 22:05:32 -0500

    Course class skeleton

---
+++ src/main/java/org/liz/dto/Course.java
@@ -0,0 +1,40 @@
+package org.liz.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Eliza Toma
+ * <p>
+ * Course
+ */
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Course {
+    private static final int MAX_STUDENT_NUM = 5;
+    private static int nextId = 1;
+
+    private String courseName;
+    private double credit;
+    private String id;
+    private Student[] students;
+    private Department department;
+    private int studentNum;
+    private Teacher teacher;
+
+    public Course(String courseName, double credit, Department department) {
+        this.courseName = courseName;
+        this.credit = credit;
+        this.department = department;
+        students = new Student[MAX_STUDENT_NUM];
+        studentNum = 0;
+        id = String.format("C%03d", nextId++);
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+}
