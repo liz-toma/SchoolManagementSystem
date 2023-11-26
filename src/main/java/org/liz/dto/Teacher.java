3abdfe986ae2896624274d0ef253e83ddee2f17e
liz-toma <2369522@edu.vaniercollege.qc.ca>
2023-11-25 22:07:47 -0500

    Teacher class skeleton

---
+++ src/main/java/org/liz/dto/Teacher.java
@@ -0,0 +1,34 @@
+package org.liz.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Eliza Toma
+ * <p>
+ * Teacher
+ */
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Teacher {
+    private static int nextId = 1;
+
+    private String fname;
+    private String lname;
+    private Department department;
+    private String id;
+
+    public Teacher(String fname, String lname, Department department) {
+        this.fname = fname;
+        this.lname = lname;
+        this.department = department;
+        id = String.format("T%03d", nextId++);
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+}
